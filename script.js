aa97d524b57e0c3bc926b1950f824cfd4163bbfc
Siddharth Kishan <91934415+sidkishan@users.noreply.github.com>
2023-03-11 16:25:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let yourArray = ["Siddharth", 7, true, false, 1];
 let li = document.getElementById("myList");
-data.forEach((item)=>{
-  let li = document.createElement("li");
+yourArray.forEach((item)=>{
+  let list = document.createElement("li");
   li.innerText = item;
   list.appendChild(li);
 })
