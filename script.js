b39067c32d765a4878d981a37495738752814ff1
Siddharth Kishan <91934415+sidkishan@users.noreply.github.com>
2023-03-11 16:24:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-let yourArray = ["Siddharth", 7, true];
+let yourArray = ["Siddharth", 7, true, false, 1];
+let li = document.getElementById("myList");
+data.forEach((item)=>{
+  let li = document.createElement("li");
+  li.innerText = item;
+  list.appendChild(li);
+})
